de55a6cbb0a16509c429b74fd2d5f708bc5c004c
uimm-charleville <125017671+uimm-charleville@users.noreply.github.com>
2023-05-25 10:53:37 +0200

    Update Person.php

--- class/Person.php
+++ class/Person.php
@@ -1,45 +1,69 @@
 <?php
 
 class Person {
+
     private string $name;
     private int $age;
-
-    public function __construct(string $name, int $age) {
+    
+    public function __construct(string $name, int $age)
+    {
         $this->name = $name;
         $this->age = $age;
     }
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function setName(string $name): void {
-        $this->name = $name;
-    }
-
-    public function getAge(): int {
+    public function getAge(): int
+    {
         return $this->age;
     }
 
-    public function setAge(int $age): void {
-        if ($age >= 0) {
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setAge(int $age): void
+    {
+        if($age < 0)
+        {
+            throw new InvalidArgumentException("Attribut non mis à jour => erreur de paramètres d'entrée : l'âge doit être un entier positif");
+        }
+        else if($age > 120)
+        {
+            throw new InvalidArgumentException("Attribut non mis à jour : la Person ne peut avoir plus de 100 ans");
+        }
+        else
+        {
             $this->age = $age;
-        } else {
-            throw new InvalidArgumentException("L'âge doit être un entier positif");
         }
     }
 
-    public function sayHello(): void {
-        echo "Bonjour, mon nom est {$this->name} et j'ai {$this->age} ans.\n";
+    public function introduce(): string
+    {
+        return "Bonjour, je m'appelle {$this->name} et j'ai " . $this->age . " ans";
     }
 
     public function calculateAgeInMonths(): int {
         return $this->age * 12;
     }
 
-    public function isAdult(): bool {
-        $adultAgeThreshold = 18;
-        return $this->age >= $adultAgeThreshold;
+    public function isAdult() {
+        if($this->age >= 18)
+        {
+            return "Robert est adulte";
+        }
+        return self::isChild();
     }
+
+    public function isChild(): string {
+        return "Robert n'est pas adulte";
+    }
+
+
 }
+
 ?>
